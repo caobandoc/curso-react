d73ba08bf98f26701a9be86139b7e7fbc9c6bfa7
caobandoc <obandocarlosandres@gmail.com>
2022-06-27 18:04:48 -0500

    Organizacion y Persistencia de datos con Local Storage

---
+++ src/App/index.js
@@ -0,0 +1,69 @@
+//import './App.css';
+import React from 'react';
+import { AppUI } from './AppUI';
+
+// const defaultTodos = [
+//   {text:'Comprar cerveza', completed:true},
+//   {text:'Cortar cebolla', completed:false},
+//   {text:'Tomar curso', completed:false},
+//   {text:'Llorar con la llorona', completed:false},
+// ];
+
+function App() {
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  }else{
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1){
+    searchedTodos = todos;
+  }else{
+    searchedTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()));
+  }
+
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (text) => {
+    let todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    let todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  }
+
+  return (
+    <AppUI 
+      totalTodos = {totalTodos}
+      completedTodos = {completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  );
+}
+
+export default App;
